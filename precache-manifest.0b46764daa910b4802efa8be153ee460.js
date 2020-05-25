f2840b87ae1976c2e27e1eb01b36e32c99936ef6
RobertTk1 <rtopkiffeur1@gmail.com>
2020-05-25 18:45:25 -0500

    Updates

---
+++ precache-manifest.0b46764daa910b4802efa8be153ee460.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c561723d2d0b065ba944d5cd35aaea",
+    "url": "/My-portfolio/index.html"
+  },
+  {
+    "revision": "68a1145648520d092515",
+    "url": "/My-portfolio/static/css/main.538ac8ed.chunk.css"
+  },
+  {
+    "revision": "79ae6e51e786664168cc",
+    "url": "/My-portfolio/static/js/2.74e43359.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-portfolio/static/js/2.74e43359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a1145648520d092515",
+    "url": "/My-portfolio/static/js/main.a455550d.chunk.js"
+  },
+  {
+    "revision": "6258dbc101afc6884e0b",
+    "url": "/My-portfolio/static/js/runtime-main.2c3a8f7b.js"
+  },
+  {
+    "revision": "7dd5a7fa424c357058fb707948fd3a78",
+    "url": "/My-portfolio/static/media/portfolio.7dd5a7fa.PNG"
+  },
+  {
+    "revision": "3857dc17bc3e74c0aa807f06429e43d3",
+    "url": "/My-portfolio/static/media/tundra.3857dc17.jpg"
+  },
+  {
+    "revision": "82189d8a1f8b81363f12e2ebb191fdd5",
+    "url": "/My-portfolio/static/media/weatherio.82189d8a.PNG"
+  }
+]);
