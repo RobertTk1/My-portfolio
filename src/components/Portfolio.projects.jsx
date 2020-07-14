2db26b7f88d9d1a7508c921d75898d570571b1fe
RobertTk1 <rtopkiffeur1@gmail.com>
2020-07-14 15:41:00 -0500

    Updated project descriptions

--- src/components/Portfolio.projects.jsx
+++ src/components/Portfolio.projects.jsx
@@ -20,28 +20,10 @@ import exploore from "../media/exploore.PNG";
 
 let projects = [
   {
-    name: "weather.io",
-    image: weather,
-    about:
-      "This is a responsive web app. It provides weather information and makes use of the openwether api. It has a dynamic background based on the data it receives.",
-    stack: ["HTML", "CSS", "JSX", "REACT", "_Hooks"],
-    github: "https://github.com/RobertTk1/weather-app/tree/master",
-    website: "https://roberttk1.github.io/weather-app/",
-  },
-  {
-    name: "Tundra",
-    image: tundra, //Imported image
-    about:
-      "Tundra is a responsive one page website. It is an easy to navigate business website with a fun hidden secret.",
-    stack: ["HTML", "CSS", "Bootstrap"],
-    github: "https://github.com/RobertTk1/Tundra/tree/master",
-    website: "https://roberttk1.github.io/Tundra/",
-  },
-  {
     name: "Exploore",
     image: exploore, //Imported image
     about:
-      "This is a fully responsive website for searching through text. This makes use of preloaded text and regular expressions. it was a good practice on getting better at writing different functions.",
+      "A fully responsive website used for searching through text based on a complex searching algorithm built using JS functions. It was a good practice for writing different functions",
     stack: ["HTML", "SASS", "Bootstrap", "Javascript"],
     github: "https://github.com/RobertTk1/Exploore/tree/master",
     website: "https://roberttk1.github.io/Exploore/",
@@ -50,16 +32,34 @@ let projects = [
     name: "CAPTIVATE",
     image: captivate, //Imported image
     about:
-      "This is a fully responsive business website. It was designed by W3 Layouts and was fully built by me. I used this to get better at bringing static designs to life.",
+      "This is a fully responsive business website that contains various sections, prepared and organized to be integrated with custom back-end platforms to serve the content dynamically",
     stack: ["HTML", "SASS", "JSX", "React", "_Router", "_Hooks"],
     github: "https://github.com/RobertTk1/Captivate/tree/master",
     website: "https://roberttk1.github.io/Captivate/",
   },
   {
+    name: "weather.io",
+    image: weather,
+    about:
+      "A fully responsive web app integrated with the Openweather API to pull in data such as temperature and weather conditions. It has a dynamic background based on the data it receives",
+    stack: ["HTML", "CSS", "JSX", "JSON", "REACT", "_Hooks"],
+    github: "https://github.com/RobertTk1/weather-app/tree/master",
+    website: "https://roberttk1.github.io/weather-app/",
+  },
+  {
+    name: "Tundra",
+    image: tundra, //Imported image
+    about:
+      "This is a fully responsive one-page business website that can be used as a presentation website. It has a well-structured code that can be easily integrated with a back-end interface to get the content dynamically",
+    stack: ["HTML", "CSS", "Bootstrap"],
+    github: "https://github.com/RobertTk1/Tundra/tree/master",
+    website: "https://roberttk1.github.io/Tundra/",
+  },
+  {
     name: "Portfolio",
     image: portfolio, //Imported image
     about:
-      "This is the curent site you are on. It is responsive and was built from the ground up. Building it was a fun process ",
+      "This is the curent site you are on. It is fully responsive and was built from the ground up. Building it was a fun process ",
     stack: ["HTML", "SASS", "JSX", "JavaScript", "REACT", "_Scroll"],
     github: "https://github.com/RobertTk1/My-portfolio/tree/master",
     website: "#",
