0284c8b30bedfa40674e40e4dba0a28c65059650
RobertTk1 <rtopkiffeur1@gmail.com>
2020-07-14 16:01:44 -0500

    Added better description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,20 +10,20 @@ export default function About() {
 
       <div className="about-info">
         <p>
-          Hi, my name is Robert Ekpai. I am a Self-motivated Software Developer.
-          Currently I am focused on front end with plans to evolve into a full
-          stack developer. I don't like problems, so I enjoy solving them. I
-          strongly believe in the power of knowledge and teamwork. As an
-          engineer I like learning new cutting-edge technologies, I am curious
-          and always eager to know what the next step is. I am multilingual and
-          fluent in all the languages I know (English, French, Ewe and Kabye). I
-          enjoy learning about self-improvement, and I am always thankful for
-          the opportunities I get.
+          I’m a solution-focused, self-motivated, and technically competent
+          Front-End Developer, with a good knowledge of databases and other
+          back-end technologies. I believe our job is about delivering values,
+          decreasing costs, and building solutions that perform. Matured by the
+          experience gained in implementing various projects, I think there is
+          no absolute right or wrong technology/stack, but the right tool for
+          the specific job. Also, I’m willing to learn cutting edge technologies
+          and modern programming techniques to address technical issues and
+          situations according to the latest trends and recommendations.
         </p>
         <h6>Favorite quote</h6>
         <span>
-          “You can learn anything you need to learn to achieve any goal you set
-          for yourself” ~ Brian Tracy
+          “The biggest goal can be accomplished if broken down into enough small
+          steps” ~ Henry Ford
         </span>
       </div>
       <div className="about-links">
